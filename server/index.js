db8115e1c12435b941b344a0047e39dccecb6975
Muskaan Bhutani <muskaan.bhutani@linkmobility.com>
2019-08-31 09:45:18 +0200

    remove unwanted code

--- server/index.js
+++ server/index.js
@@ -4,15 +4,6 @@ const server = express();
 
 const port = 4000;
 
-//CORS Middleware
-server.use(function (req, res, next) {
-  //Enabling CORS
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
-  next();
-  });
-
 server.listen(port, () => {
     console.log(`Server listening at ${port}`);
 });
@@ -29,10 +20,6 @@ server.get(['/api/ships', '/api/ships/:query'], (req, res) => {
     const matches = ships.filter(ship =>
         ship.name.toLowerCase().includes(query.toLowerCase())
     );
-
-    res.header('Access-Control-Allow-Origin', '*');
-  //res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
 
     res.json(matches);
 });
